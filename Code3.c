b190e34dfd71b7b9064d3ed7706b862239bd38b1
Sujal Thapa1 <sujalmangar304@gmail.com>
2025-01-06 14:55:56 +0530

    Code4 updated

--- Code3.c
+++ Code3.c
@@ -5,11 +5,8 @@
  * This function iterates through numbers from 0 to 100 and prints
  * only the even numbers to the console.
  *
- * @param argc Number of command-line arguments.
- * @param argv Array of command-line argument strings.
- * @return 0 on successful execution.
  */
-int main(int argc, char const *argv[])
+int main()
 {
     int num=0;
     while (num<=100){
---
+++ Code4.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+/**
+ * @brief Prints a right-aligned triangle pattern of asterisks
+ *
+ * This function prints a right-aligned triangle pattern of asterisks
+ * with a height of 5 rows. Each row is padded with spaces on the left
+ * to create the right-aligned effect.
+ *
+ * @param argc Number of command-line arguments
+ * @param argv Array of command-line argument strings
+ * @return 0 on successful execution
+ */
+int main(int argc, char const *argv[])
+{
+    int n=5;
+    int i,j;
+    for(i=1;i<=n;i++){
+        for(j=n;j>=1;j--){
+            if(i>=j){
+                printf("* ");
+            }else{
+                printf(" ");
+            }   
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
